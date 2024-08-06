ec5690f5ad13a785c48b995c1ebae31903bb534a
Ryan Gupta <ryangupta8@gmail.com>
2024-08-06 13:20:39 -0500

    added joystick logic and correctly set vel cmd

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -32,8 +32,8 @@ class Go1Env():
 
     def __init__(self):
         self.q_stand = np.array([-0.1, 0.8, -1.5, 0.1, 0.8, -1.5, -0.1, 1.0, -1.5, 0.1, 1.0, -1.5])
-        self.vel_cmd = np.zeros(3) # the policy input, used in concatenated obs
-        self.a_cmd = np.zeros(12) # accel cmd from policy
+        self.vel_cmd = np.zeros(3, dtype=float) # the policy input, used in concatenated obs
+        self.a_cmd = np.zeros(12, dtype=float) # accel cmd from policy
 
         self.q = np.zeros(12)
         self.dq = np.zeros(12)
@@ -63,7 +63,7 @@ class Go1Env():
 
         # PD Gains for robot
         # Kp=20, Kd=0.5, Ka=0.25
-        self.kp = 20
+        self.kp = 30
         self.kd = 0.5
         self.ka = 0.25
         # Robot Startup object
@@ -96,14 +96,15 @@ class Go1Env():
         c = q_vec * q_vec * gravity
         return a - b + c
 
-    def step(self, vel_cmd, a_cmd):
-        self.vel_cmd = vel_cmd
+    def step(self, a_cmd):
+        # self.vel_cmd = vel_cmd
         self.a_cmd = a_cmd
 
         step_time = time.time()
         
         while ((time.time() - step_time) < POLICY_STEP):
             self.run_robot()
+            # print("POLICY STEP", (time.time() - step_time))
 
         # print("self.q.shape = ", self.q.shape)
         # print("self.dq.shape = ", self.dq.shape)
@@ -126,11 +127,90 @@ class Go1Env():
         # Body quaternion, normalized, (w,x,y,z)
         quat = np.array([self.lowstate.imu.quaternion]) 
         self.projected_gravity = self.quat_rot_inv(quat, np.array([0,0,-9.81]))
-    
+
+        wirelessRemote = self.lowstate.wirelessRemote
+        ## Left Stick forward/backward == xdot command
+        # 23 is left stick forward/backwards (can get direction 58-63 / 186-191)
+        # 22 is left stick foward/backwards (can get intensity 129-0jump256-0-256jump0-128)
+        # 58-63 --> pushing forward (62 to 63 is where we go from 256 to 0)
+        # 186-191 --> pushing backwards (190 to 191 is where we go from 256 to 0)
+        # if left stick being pressed forward
+        #print(type(wirelessRemote[23]))
+        #print(type(float(wirelessRemote[22])))
+        print(float(wirelessRemote[22]))
+        if (59 <= wirelessRemote[23] <= 63):
+            if wirelessRemote[23] != 63:
+                intensity = float(wirelessRemote[22]) / 384.
+            elif wirelessRemote[23] == 63:
+                intensity = float((wirelessRemote[22] + 256.)) / 384.
+            print(intensity)
+            self.vel_cmd[0] = intensity
+        # if left stick being pressed backward
+        elif (187 <= wirelessRemote[23] <= 191):
+            if wirelessRemote[23] != 191:
+                intensity = float(wirelessRemote[22]) / 384.
+            elif wirelessRemote[23] == 191:
+                intensity = float((wirelessRemote[22] + 256.)) / 384.
+            self.vel_cmd[0] = -intensity
+        else:
+            self.vel_cmd[0] = 0.0
+
+        ## Left Stick left/right == ydot command
+        # 7 is left stick left/right (can get direction 58-63 / 188-191)
+        # 6 is left stick left/right (can get intensity)
+        # 58-63 --> pushing right (62 to 63 is where we go from 256 to 0)
+        # 188-191 (controller slightly less sensitive) 
+            # --> pushing left (190 to 191 is where we go from 256 to 0)
+        # if left stick being pushed right
+        if (59 <= wirelessRemote[7] <= 63):
+            if wirelessRemote[7] != 63:
+                intensity = float(wirelessRemote[6] / 384)
+            elif wirelessRemote[7] == 63:
+                intensity = float((wirelessRemote[6] + 256) / 384)
+            self.vel_cmd[1] = -intensity
+        # if left stick beign pushed left
+        elif (188 <= wirelessRemote[7] <= 191):
+            if wirelessRemote[7] != 191:
+                intensity = float(wirelessRemote[6] / 384)
+            elif wirelessRemote[7] == 191:
+                intensity = float((wirelessRemote[6] + 256) / 384)
+            self.vel_cmd[1] = intensity
+        else:
+            self.vel_cmd[1] = 0.0
+
+        ## Right Stick left/right == omega command
+        # 11 is right stick left/right (can get direction) 60; 188
+        # 10 is right stick left/right (can get intensity) 
+        # if right stick being pushed right
+        if (60 <= wirelessRemote[11] <= 63):
+            if wirelessRemote[11] != 63:
+                intensity = float(wirelessRemote[10] / 384)
+            elif wirelessRemote[11] == 63:
+                intensity = float((wirelessRemote[10] + 256) / 384)
+            self.vel_cmd[2] = -intensity
+        # if right stick beign pushed left
+        elif (188 <= wirelessRemote[11] <= 191):
+            if wirelessRemote[11] != 191:
+                intensity = float(wirelessRemote[10] / 384)
+            elif wirelessRemote[11] == 191:
+                intensity = float((wirelessRemote[10] + 256) / 384)
+            self.vel_cmd[2] = intensity
+        else:
+            self.vel_cmd[2] = 0.0
+
+        print("velocity command = ", self.vel_cmd)
+
+        ## Unused
+        # 39 appears to roughly be the right forwards/backwards
+        # 15 is right stick forward/backwards (can get direction)
+        # 14 is right stick forward/backwards (can maybe get intensity)
+        # 21 is left strick diagonals but unclear mapping
+
     def run_robot(self):
         cur_time = time.time()
         control_time = time.time()
         while ((time.time() - control_time) < CONTROL_STEP):
+            # print((time.time() - control_time))
             pass
 
         self.get_obs()
@@ -140,15 +220,15 @@ class Go1Env():
         #     self.lowcmd.motorCmd[motor_id].q = self.q_stand[motor_id] + self.ka*self.a_cmd[motor_id] # q_des
         #     self.lowcmd.motorCmd[motor_id].Kp = self.kp # kp
         ## Option 2
-        # for motor_id in range(12):
-        #     self.lowcmd.motorCmd[motor_id].q = self.q_stand[motor_id] # q_des
-        #     self.lowcmd.motorCmd[motor_id].Kp = self.kp # kp
-        #     self.lowcmd.motorCmd[motor_id].dq = 0 # dq_des
-        #     self.lowcmd.motorCmd[motor_id].Kd = self.kd # kd
-        #     self.lowcmd.motorCmd[motor_id].tau = self.kp*self.ka*self.a_cmd[motor_id]  # FF torque
-        ## Option 3
         for motor_id in range(12):
-           self.lowcmd.motorCmd[motor_id].tau = self.kp*(self.q_stand[motor_id] - self.q[motor_id]) + self.kd*(-self.dq[motor_id]) + self.kp*self.ka*self.a_cmd[motor_id]  # FF torque
+            self.lowcmd.motorCmd[motor_id].q = self.q_stand[motor_id] # q_des
+            self.lowcmd.motorCmd[motor_id].Kp = self.kp # kp
+            self.lowcmd.motorCmd[motor_id].dq = 0 # dq_des
+            self.lowcmd.motorCmd[motor_id].Kd = self.kd # kd
+            self.lowcmd.motorCmd[motor_id].tau = self.kp*self.ka*self.a_cmd[motor_id]  # FF torque
+        ## Option 3 ## NOT STABLE
+        # for motor_id in range(12):
+        #    self.lowcmd.motorCmd[motor_id].tau = self.kp*(self.q_stand[motor_id] - self.q[motor_id]) + self.kd*(-self.dq[motor_id]) + self.kp*self.ka*self.a_cmd[motor_id]  # FF torque
 
         self.safe.PowerProtect(self.lowcmd, self.lowstate, 1)
         self.udp.SetSend(self.lowcmd)
@@ -163,7 +243,7 @@ def main():
     steps = 0
     obs_history = np.zeros((42, H))
 
-    vel_cmd = np.array([0,0,0]) # policy input
+    # vel_cmd = np.array([0,0,0]) # policy input
     q_cmd = np.zeros(12)# policy output
     ## todo 
     #     - init obs_history from real readings
@@ -171,7 +251,7 @@ def main():
 
     while steps < 1000:
 
-        obs = env.step(vel_cmd, q_cmd)
+        obs = env.step(q_cmd)
         obs = np.expand_dims(obs, axis=1)
         # print("obs_history.shape = ", obs_history.shape)
         # print("obs.shape = ", obs.shape)
