dc5110ad96a59f802bf2bb898d5695919594ed81
KyleM73 <kyle.morgenstein@gmail.com>
2024-08-27 11:48:08 -0500

    onnx models

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from src import RealRobotInit
 from third_party import robot_interface
+import onnx_models
 
-__all__ = ["RealRobotInit", "robot_interface"]
+__all__ = ["RealRobotInit", "robot_interface", "onnx_models"]
