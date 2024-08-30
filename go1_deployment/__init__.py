8387d511defffc78c937084424095ba92056d616
KyleM73 <kyle.morgenstein@gmail.com>
2024-08-30 13:49:21 -0500

    tmp import fix

--- go1_deployment/__init__.py
+++ go1_deployment/__init__.py
@@ -1,10 +1,10 @@
 from go1_deployment.third_party import robot_interface
 from go1_deployment.scripts import CONTROL_STEP, POLICY_STEP, OBS_LEN, H
-from go1_deployment.src import RealRobotInit
+# from go1_deployment.src import RealRobotInit
 import go1_deployment.onnx_models
 
 __all__ = [
-    "RealRobotInit",
+    # "RealRobotInit",
     "robot_interface",
     "onnx_models",
     "CONTROL_STEP",
