abdb7f4c0f3609514e3db38219072e5b9f2838eb
KyleM73 <kyle.morgenstein@gmail.com>
2024-09-05 17:51:43 -0500

    fix init

--- go1_deployment/__init__.py
+++ go1_deployment/__init__.py
@@ -1,10 +1,7 @@
 import os
 
-from go1_deployment.third_party import robot_interface
-
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 __all__ = [
-    "robot_interface",
     "DIR_PATH",
 ]
